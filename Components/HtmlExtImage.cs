2831ebb2ef141f52a0accad6e970e5a3ee465d19
Lukasz Bekus <beckon@interia.pl>
2020-02-24 12:39:53 +0100

    image helper added

---
+++ Components/HtmlExtImage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DahaimMVC
+{
+    public static class HtmlExtImage
+    {
+        public static MvcHtmlString Image(this HtmlHelper htmlHrlper, 
+                                          string src,
+                                          string altText,
+                                          string cssClass,
+                                          object htmlAtributes = null)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.MergeAttribute("src", src);
+            tb.MergeAttribute("alt", altText);
+
+            tb.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAtributes)); 
+
+            if (!string.IsNullOrWhiteSpace(cssClass))
+            {
+                tb.AddCssClass(cssClass);
+            }
+
+            return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
