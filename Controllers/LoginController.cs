d763e552a179354e36aba98672189a5b4c18f4e5
Lukasz Bekus <beckon@interia.pl>
2020-02-26 13:16:55 +0100

    repozytorium

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -13,116 +13,12 @@ namespace DahaimMVC.Controllers
 {
     public class LoginController : Controller
     {
-        private DahaimAppContext db = new DahaimAppContext();
+        private readonly DahaimAppContext db = new DahaimAppContext();
 
-        // GET: Login
-        public ActionResult Index()
-        {
-            return View(db.Users.ToList());
-        }
-
-        // GET: Login/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserModel userModel = db.Users.Find(id);
-            if (userModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userModel);
-        }
-
-        // GET: Login/Create
-        public ActionResult Create()
+        public ActionResult AddOrEdit()
         {
             return View();
         }
 
-        // POST: Login/Create
-        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
-        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Login,Password")] UserModel userModel)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Users.Add(userModel);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(userModel);
-        }
-
-        // GET: Login/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserModel userModel = db.Users.Find(id);
-            if (userModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userModel);
-        }
-
-        // POST: Login/Edit/5
-        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
-        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Login,Password")] UserModel userModel)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(userModel).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(userModel);
-        }
-
-        // GET: Login/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserModel userModel = db.Users.Find(id);
-            if (userModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userModel);
-        }
-
-        // POST: Login/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            UserModel userModel = db.Users.Find(id);
-            db.Users.Remove(userModel);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
---
+++ Models/IUsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DahaimMVC.Models
+{
+    public interface IUsersRepository
+    {
+        void AddUserModel(UserModel userModel);
+    }
+}
