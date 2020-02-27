61243729d1629ad72c16dcc2f9eb1f95cc6024cd
Lukasz Bekus <beckon@interia.pl>
2020-02-27 09:14:49 +0100

    dodany interface

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,3 +1,5 @@
+using Autofac;
+using Autofac.Integration.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +12,11 @@ namespace DahaimMVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
+
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ Models/IUserData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DahaimMVC.Models
+{
+    public interface IUserData
+    {
+        IEnumerable<User> GetAll();
+        User Get(int id);
+        void Add(User restaurant);
+        void Edit(User restaurant);
+        void Delete(User restaurant);
+    }
+}
