b9ec55b77fe9c30695259d2e5739019da545c1fd
Lukasz Bekus <beckon@interia.pl>
2020-03-02 13:50:09 +0100

    logowanie admin prawie dziala sesja teraz

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using DahaimMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DahaimMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private Admin admin;
+
+        public AdminController()
+        {
+            admin =  new Admin();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin adminModel)
+        {
+            if ((adminModel.AdminName == admin.AdminName) && (adminModel.AdminPassword == admin.AdminPassword))
+            {
+                return RedirectToAction("Index", "ControlPanel");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Admin/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DahaimMVC.Models
+{
+    public class Admin
+    {
+
+        public Admin()
+        {
+            AdminName = "admin";
+            AdminPassword = "admin";
+        }
+        [Required]
+        public string AdminName { get; set; }
+        [Required]
+        public string AdminPassword { get; set; }
+    }
+}
