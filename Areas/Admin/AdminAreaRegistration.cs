561a581e55189120ce982326f1cda84c0db8ab8e
Lukasz Bekus <beckon@interia.pl>
2020-03-11 11:15:59 +0100

    dalej

--- Areas/Admin/AdminAreaRegistration.cs
+++ Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace DahaimMVC.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
