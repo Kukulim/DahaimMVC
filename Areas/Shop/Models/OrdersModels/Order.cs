de16e8997a92741d1d52f19214d6a7a982f122bf
Lukasz Bekus <beckon@interia.pl>
2020-04-04 15:14:38 +0200

    Jeszcze w admin panel zamowienia

--- Areas/Shop/Models/OrdersModels/Order.cs
+++ Areas/Shop/Models/OrdersModels/Order.cs
@@ -12,36 +12,51 @@ namespace DahaimMVC.Areas.Shop.Models.OrdersModels
         public int OrderId { get; set; }
         [ScaffoldColumn(false)]
         public string Username { get; set; }
-        [Required(ErrorMessage = "First Name is required")]
-        [DisplayName("First Name")]
+
+        [Required(ErrorMessage = "Imię jest wymagane")]
+        [DisplayName("Imię:")]
         [StringLength(160)]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "Last Name is required")]
-        [DisplayName("Last Name")]
+
+        [Required(ErrorMessage = "Nazwisko jest wymagane")]
+        [DisplayName("Nazwisko:")]
         [StringLength(160)]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Address is required")]
+
+        [DisplayName("Adres:")]
+        [Required(ErrorMessage = "Adres jest wymagany")]
         [StringLength(70)]
         public string Address { get; set; }
-        [Required(ErrorMessage = "City is required")]
+
+        [DisplayName("Miasto:")]
+        [Required(ErrorMessage = "Nazwisko jest wymagane")]
         [StringLength(40)]
         public string City { get; set; }
-        [Required(ErrorMessage = "State is required")]
+
+        [DisplayName("Województwo:")]
+        [Required(ErrorMessage = "Województwo jest wymagane")]
         [StringLength(40)]
         public string State { get; set; }
-        [Required(ErrorMessage = "Postal Code is required")]
-        [DisplayName("Postal Code")]
+
+
+        [Required(ErrorMessage = "Kod pocztowy jest wymagany")]
+        [DisplayName("Kod pocztowy:")]
         [StringLength(10)]
         public string PostalCode { get; set; }
-        [Required(ErrorMessage = "Country is required")]
+
+        [DisplayName("Kraj:")]
+        [Required(ErrorMessage = "Kraj jest wymagany")]
         [StringLength(40)]
         public string Country { get; set; }
-        [Required(ErrorMessage = "Phone is required")]
+
+        [DisplayName("Telefon:")]
+        [Required(ErrorMessage = "Telefon jest wymagany")]
         [StringLength(24)]
         public string Phone { get; set; }
-        [Required(ErrorMessage = "Email Address is required")]
-        [DisplayName("Email Address")]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email is is not valid.")]
+
+        [Required(ErrorMessage = "Email jest wymagany")]
+        [DisplayName("Email:")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email nie spełnia wymogów")]
         public string Email { get; set; }
         [ScaffoldColumn(false)]
         public decimal Total { get; set; }
